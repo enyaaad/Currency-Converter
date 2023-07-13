a844e66bf9c3b78d2017fed570e99c8dc890ed04
enyaaad <enindima1@gmail.com>
2023-07-14 01:01:09 +0700

    end version with correct price diff displaying

--- src/app/converter-page/converter-page.component.ts
+++ src/app/converter-page/converter-page.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core'
 import { ApiConnService } from '../services/apiconn.service'
-import { Observable } from 'rxjs'
+import { BehaviorSubject, map, Observable, of, pairwise } from 'rxjs'
 import { Currency } from '../models/currency'
 
 @Component({
@@ -12,15 +12,65 @@ import { Currency } from '../models/currency'
 export class ConverterPageComponent implements OnInit {
   constructor (private readonly apiService: ApiConnService) {}
   data$: Observable<Currency[]> = new Observable<[]>()
+  // for debug
+  // results: BehaviorSubject<Currency[]> = new BehaviorSubject<Currency[]>([])
+  // data$: Observable<Currency[]> = this.results.asObservable()
 
   connect (): void {
     this.apiService.toggleParams()
   }
+  // for debug
+  // imit (): void {
+  //   setTimeout(() => {
+  //     this.results.next(data)
+  //   }, 5000)
+  //   setTimeout(() => {
+  //     this.results.next(data2)
+  //   }, 10000)
+  //   setTimeout(() => {
+  //     this.results.next(data)
+  //   }, 15000)
+  //   setTimeout(() => {
+  //     this.results.next(data2)
+  //   }, 20000)
+  // }
 
   ngOnInit (): void {
+    // for debug
+    // this.imit()
+
     this.apiService.initFetching()
 
     this.data$ = this.apiService.data$
+
+    this.data$.pipe(
+      pairwise(),
+      map(([prev, curr]) => {
+        const diffs: number[] = this.calculateDiff(curr, prev)
+        curr.forEach((el, index) => {
+          el.diff = Math.floor((diffs[index] * 1000) / 1000)
+        })
+      })
+    ).subscribe()
+  }
+
+  calculateDiff (current: Currency[], previous: Currency[]): number[] {
+    if (previous.length > 0) {
+      const result: number[] = []
+
+      for (let i = 0; i < current.length; i++) {
+        const obj1 = current[i]
+        if (previous[i].result === undefined) {
+          previous[i].result = 0
+        }
+        const obj2 = previous[i]
+
+        const subtractedValue = obj1.result - obj2.result
+        result.push(subtractedValue)
+      }
+      return result
+    }
+    return []
   }
 
   protected readonly Math = Math
--- src/app/models/currency.ts
+++ src/app/models/currency.ts
@@ -2,7 +2,7 @@ export interface Currency {
   success: boolean
   query: Query
   result: number
-  diff?: number | 0
+  diff: number
 }
 
 export interface Query {
@@ -11,16 +11,69 @@ export interface Query {
   amount: number
 }
 
-// {
-//   "success": true,
-//   "query": {
-//   "from": "USD",
-//     "to": "RUB",
-//     "amount": 1
-// },
-//   "info": {
-//   "timestamp": 1689096783,
-//     "quote": 90.250014
-// },
-//   "result": 90.250014
-// }
+//for debug
+// export const data: Currency[] = [
+//   {
+//     success: true,
+//     query: {
+//       from: 'USD',
+//       to: 'RUB',
+//       amount: 1
+//     },
+//     result: 98.2,
+//     diff: 0
+//   },
+//   {
+//     success: true,
+//     query: {
+//       from: 'JPN',
+//       to: 'RUB',
+//       amount: 1
+//     },
+//     result: 32.2,
+//     diff: 0
+//   },
+//   {
+//     success: true,
+//     query: {
+//       from: 'BYN',
+//       to: 'RUB',
+//       amount: 1
+//     },
+//     result: 16.2,
+//     diff: 0
+//   }
+// ]
+//
+// export const data2: Currency[] = [
+//   {
+//     success: true,
+//     query: {
+//       from: 'USD',
+//       to: 'RUB',
+//       amount: 1
+//     },
+//     result: 77.2,
+//     diff: 0
+//   },
+//   {
+//     success: true,
+//     query: {
+//       from: 'JPN',
+//       to: 'RUB',
+//       amount: 1
+//     },
+//     result: 44.2,
+//     diff: 0
+//   },
+//   {
+//     success: true,
+//     query: {
+//       from: 'BYN',
+//       to: 'RUB',
+//       amount: 1
+//     },
+//     result: 23.2,
+//     diff: 0
+//   }
+// ]
